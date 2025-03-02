97f4b27af244e736f7ad61f928e849a8aa7fe834
ankshah131 <61028925+ankshah131@users.noreply.github.com>
2025-03-02 13:27:21 -0600

    Update severity_analysis.py

--- app/components/severity_analysis.py
+++ app/components/severity_analysis.py
@@ -30,7 +30,7 @@ class SeverityAnalysis:
         # Create a Folium map centered on Los Angeles
         m = folium.Map(location=[34.18716861471849, -118.32657647829909], 
                       zoom_start=11, min_zoom=6,max_zoom=14, max_bounds=True,
-                      tiles="cartodbpositron")
+                      tiles="OpenStreetMap")
         
         # Add pre-burn layer
         folium.TileLayer(
