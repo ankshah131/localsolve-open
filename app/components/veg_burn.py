1535b3dce953d3d5c0f0f2e7de339c512b63e4b9
ankshah131 <61028925+ankshah131@users.noreply.github.com>
2025-08-07 11:00:44 -0500

    Update veg_burn.py

--- app/components/veg_burn.py
+++ app/components/veg_burn.py
@@ -223,7 +223,7 @@ class VegBurn:
                 if m is not None:
                     # Make map responsive by removing fixed width
                     #folium_static(m, width=None, height=600)
-                    folium_static(m, width=1300, height=600)
+                    folium_static(m, width=1320, height=600)
         
         with tabs[1]:
             st.subheader("How Burn Severity is Measured")
